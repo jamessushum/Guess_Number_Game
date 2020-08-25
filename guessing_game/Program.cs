40aa200c25badf4338970ec33d7d39ef0480cf1c
James Su-Shum <sushumjames@gmail.com>
2020-08-25 12:13:07 -0400

    Initialized project and completed Phase 1 and 2 of program requirements

---
+++ guessing_game/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace guessing_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Guessing Game!");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine();
+            AskQuestion();
+        }
+
+        static void AskQuestion()
+        {
+            int secretNum = 42;
+            Console.Write("Guess the secret number: ");
+            string answer = Console.ReadLine().ToLower();
+            int stringToInt = Int32.Parse(answer);
+            if (stringToInt == secretNum)
+            {
+                Console.Write("You're right!");
+            }
+            else
+            {
+                Console.Write("You're wrong!");
+            }
+        }
+    }
+}
